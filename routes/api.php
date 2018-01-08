33739d51645817748bfce2e2b0ea880152fd1c4a
Gonçalo Graça Ribeiro <2151159@my.ipleiria.pt>
2018-01-08 00:14:52 +0000

    revert da eliminação de certas rotas

--- routes/api.php
+++ routes/api.php
@@ -28,14 +28,15 @@ Route::post('login', 'LoginControllerAPI@login');
 Route::post('logout','LoginControllerAPI@logout');
 
 Route::middleware('auth:api')->get('users', 'UserControllerAPI@getUsers');
-
+Route::middleware('auth:api')->put('users/block/{id}', 'UserControllerAPI@block');
+Route::middleware('auth:api')->put('users/reactivate/{id}', 'UserControllerAPI@reactivate');
+Route::middleware('auth:api')->delete('users/{id}', 'UserControllerAPI@delete');
 
 Route::middleware('auth:api')->put('admin/password','AdminControllerAPI@updatePassword');
 Route::middleware('auth:api')->put('admin/email','AdminControllerAPI@updateEmail');
 Route::middleware('auth:api')->post('admin/password/email','ResetPasswordControllerAPI@sendResetLinkEmail');
 Route::post('admin/password/reset','ResetPasswordControllerAPI@reset');
 
-//Route::middleware('auth:api')->get('images','ImageControllerAPI@getImages');
 Route::get('images','ImageControllerAPI@getImages');
 
 // o middleware admin:api não funciona porque não consegue fazer retrieve do user logo nao consegue verificar se é admin
